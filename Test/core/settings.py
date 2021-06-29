bb6eb30b19fe3ac1202e42a1bc054a0b943b5d6d
pashadark <turpavel@hotmail.com>
2021-06-30 02:25:29 +0300

    first commit for all files5

--- Test/core/settings.py
+++ Test/core/settings.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
-
+import dj_database_url
 import environ
+
 
 # fmt: off
 # NOTE `fmt` comments disable/enable Black formatting in this section
@@ -142,5 +144,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
